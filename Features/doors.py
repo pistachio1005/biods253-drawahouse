cf0dbd79d51a305c457f3f7e1679f7fe86100f83
drendon28 <drendon2882@gmail.com>
2024-01-21 14:20:10 -0800

    doors and windows

--- Features/doors.py
+++ Features/doors.py
@@ -1,1 +1,54 @@
+import turtle
 
+#draw rectangluar door
+def rectangle(custom_turtle, width, height):
+    for _ in range(2):
+        custom_turtle.forward(width)  #Move and turn turtle
+        custom_turtle.right(90)       
+        custom_turtle.forward(height)
+        custom_turtle.right(90)       
+
+#turtle graphics screen
+window = turtle.Screen()
+
+#door turtle
+door_turtle = turtle.Turtle()
+door_turtle.shape("turtle")
+door_turtle.color("brown")
+
+#door position
+door_turtle.penup()
+door_turtle.goto(0, -20)  #Adjust door position
+door_turtle.pendown()
+
+#Draw rectangular door
+rectangle(door_turtle, 60, 100)
+
+#first garage turtle
+garage1_turtle = turtle.Turtle()
+garage1_turtle.shape("turtle")
+garage1_turtle.color("gray")
+
+#Position the first garage turtle without drawing
+garage1_turtle.penup()
+garage1_turtle.goto(140, 0)  # Adjust the position as needed
+garage1_turtle.pendown()
+
+#draw first rectangular garage
+rectangle(garage1_turtle, 100, 120)
+
+#second garage turtle
+garage2_turtle = turtle.Turtle()
+garage2_turtle.shape("turtle")
+garage2_turtle.color("gray")
+
+#position the second garage turtle without drawing
+garage2_turtle.penup()
+garage2_turtle.goto(-180, 0)  # Adjust the position as needed
+garage2_turtle.pendown()
+
+#second rectangular garage
+rectangle(garage2_turtle, 100, 120)
+
+#Close the turtle graphics window when clicked
+window.exitonclick()
--- Features/windows.py
+++ Features/windows.py
@@ -1,1 +1,32 @@
+import turtle
 
+ #Draw square window
+def square_window(my_turtle):
+    for _ in range(4):
+        my_turtle.forward(50)  #Move the turtle 50 spaces
+        my_turtle.right(90)    #Turn the turtle
+
+#Create a turtle graphics screen
+window = turtle.Screen()
+
+#Create the first turtle
+turtle1 = turtle.Turtle()
+turtle1.shape("turtle")
+turtle1.color("blue")
+
+square_window(turtle1)
+
+#Create the second turtle
+turtle2 = turtle.Turtle()
+turtle2.shape("turtle")
+turtle2.color("blue")
+
+#Move the second turtle to a new position
+turtle2.penup()
+turtle2.goto(200, 0)  #Move 200 spaces to the right
+turtle2.pendown()
+
+square_window(turtle2)
+
+#Close the turtle graphics window when clicked
+window.exitonclick()
