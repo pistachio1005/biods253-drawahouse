6f3c4fa4802a25e4ce36d94cdb66d1e632085037
WenyuanChen1326 <cwyuan1010@gmail.com>
2024-01-21 14:45:12 -0800

    finished trees and green grass yard

--- main_script.py
+++ main_script.py
@@ -1,19 +1,27 @@
 import turtle as t
 from Features.outline import *
 from Features.clouds import *
+from Features.trees import *
 
 def main():
-    t.speed(10)
+    t.speed("fastest")
 
     # Set up Turtle and set canvas dimensions
     t.setup(width=1000, height=1000)
 
-
+    # Draw the yard
+    draw_filled_rectangle(-500, -500, 1000, 300, "green")
     # Draw the house outline
     draw_house(-200, -150, 400, 200, 100)
     # Draw the clouds
     draw_cloud(-300,300,40)
     draw_cloud(250,250,30)
+
+
+    # Draw trees
+    draw_tree(-400, -200, 100, 40, 120)  # Tree on the left side
+    draw_tree(300, -200, 100, 40, 120) 
+
 
     # Hide Turtle
     t.hideturtle()
